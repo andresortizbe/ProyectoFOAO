985f986b2ea7cd9cac57d914b62b9c19062b5573
andresortizbe <70553013+andresortizbe@users.noreply.github.com>
2020-10-13 18:50:29 -0500

    Update main.js

--- main.js
+++ main.js
@@ -66,7 +66,7 @@ function returnCardHTML(q)
 
 function randomize(correct,incorrect)
 {
-    var ran = Math.floor(Math.random(0,3));
+    var ran = Math.floor(Math.random(1,3));
     console.log(ran);
 
 
